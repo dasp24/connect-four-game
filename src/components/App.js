833670e52f3464e3aa5470f4c653bee5e3dffd12
Dan Parkes <daniel.parkes901@gmail.com>
2017-08-06 22:10:28 +0100

    set up App js file

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import Turn from './Turn';
+import Board from './Board';
+import Choice from './Choice';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Title />
+      <Turn />
+      <Board />
+      <Choice />
+      </div>
+    );
+  }
+}
+
+export default App;
