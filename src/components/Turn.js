5db2a2465481a3d06139114116dd4ac71fb35d86
Dan Parkes <daniel.parkes901@gmail.com>
2017-08-06 22:11:54 +0100

    added title stating who the player is

---
+++ src/components/Turn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Turn extends Component {
+  render() {
+    return (
+      <div className="Turn">
+          <h1>Player 1 to play</h1>
+      </div>
+    );
+  }
+}
+
+export default Turn;
