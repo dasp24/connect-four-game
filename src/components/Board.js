e28e0b6e93858a225b1c2c040172a6b2dbbe3633
Dan Parkes <daniel.parkes901@gmail.com>
2017-08-06 22:10:53 +0100

    added board and stylings

---
+++ src/components/Board.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+const slotDimension = 20;
+const slotStyle = {
+    width: `${slotDimension}px`,
+    height: `${slotDimension}px`,
+    borderRadius: `${slotDimension / 2}px`,
+    margin: '0 5px',
+    display: 'inline-block'
+};
+
+class Board extends Component {
+    renderSlot(slot) {
+        switch (slot) {
+            case 'x':
+                return <div style={{...slotStyle, backgroundColor: 'red' }} />;
+            case 'o':
+                return <div style={{...slotStyle, backgroundColor: 'yellow' }} />;
+            default:
+                return <div style={{...slotStyle, backgroundColor: 'white' }} />;
+        }
+    }
+
+  render() {
+    return this.props.won ? <span>'A player has won!'</span> : (
+      <div className="Board">
+        {this.props.visualBoard.map((row) => (
+          <div>
+            {row.map(this.renderSlot.bind(this))}
+            <br />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+    const visualBoard = state.board.map(row => row.map(slot => slot ? slot : '_'));
+
+    return {
+        visualBoard,
+        won: state.won
+    };
+
+// need some sort of function that represents a null in an array as something
+
+// another function that repesents x as green and y as blue or whatever colour
+}
+
+export default connect(mapStateToProps)(Board);
