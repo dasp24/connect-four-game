987790ebf8775fb4b803404a07b0566d7b336dd6
Dan Parkes <daniel.parkes901@gmail.com>
2017-08-06 22:11:36 +0100

    set up header

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="Title">
+          <h1>Connect 4 game</h1>
+      </div>
+    );
+  }
+}
+
+export default Title
