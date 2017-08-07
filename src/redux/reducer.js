5199e19efa482f439b3d0f6d9ff427d2b285f99b
Dan Parkes <daniel.parkes901@gmail.com>
2017-08-07 12:39:59 +0100

    removed function to new folder - helperFuncs

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -8,7 +8,7 @@ import {
   CHECK_WINNER
 } from './actions'
 
-// import {addDisk, checkWinner} from './helperFuncs'
+import {addDisk, checkWinner, changePlayer} from './helperFuncs'
 
 const initialState = {
   board: [
@@ -49,62 +49,6 @@ export function reducer(state = initialState, action) {
   }
 }
 
-function addDisk(state, col) {
-  const newState = { ...state
-  }
-  if (newState.won !== false) return newState
-  let row = 5;
-  while (newState.board[row][col] !== null) {
-    row--;
-    if (row < 0) return newState;
-  }
 
-  newState.board[row][col] = state.player;
-
-  return newState;
-
-}
-
-function checkWinner(board, player) {
-  const positions = [];
-   board.forEach((line, index) => {
-        line.forEach((item, jindex) => {
-            if (item === player) positions.push([index, jindex]);
-        });
-    });
-
-  let currentPos = positions.shift();
-    while (positions.length >= 3) {
-        if (board[currentPos[0] + 3] !== undefined) {
-            if (board[currentPos[0] + 1][currentPos[1]] === player && board[currentPos[0] + 2][currentPos[1]] === player && board[currentPos[0] + 3][currentPos[1]] === player) {
-                return `${player} wins`;
-            }
-        }
-
-        if (board[currentPos[0]][currentPos[1] + 3] !== undefined) {
-            if (board[currentPos[0]][currentPos[1] + 1] === player && board[currentPos[0]][currentPos[1] + 2] === player && board[currentPos[0]][currentPos[1] + 3] === player) {
-                return `${player} wins`;
-            }
-        }
-        if (board[currentPos[0]][currentPos[1] + 3] !== undefined && board[currentPos[0] + 3] !== undefined) {
-            if (board[currentPos[0] + 1][currentPos[1] + 1] === player && board[currentPos[0] + 2][currentPos[1] + 2] === player && board[currentPos[0] + 3][currentPos[1] + 3] === player) {
-                return `${player} wins`;
-            }
-        }
-        if (board[currentPos[0]][currentPos[1] - 3] !== undefined && board[currentPos[0] + 3] !== undefined) {
-            if (board[currentPos[0] + 1][currentPos[1] - 1] === player && board[currentPos[0] + 2][currentPos[1] - 2] === player && board[currentPos[0] + 3][currentPos[1] - 3] === player) {
-                return `${player} wins`;
-            }
-        }
-        currentPos = positions.shift();
-    }
-      return false;
-}
-
-function changePlayer(player, won) {
-  if (won !== false) return player;
-  player = (player === 'x') ? 'o':'x';
-  return player;
-}
 
 export default createStore(reducer);
