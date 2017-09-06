f7e834a08180892515f848a2462f00be59f48b0c
Dan Parkes <daniel.parkes901@gmail.com>
2017-09-06 14:50:12 +0100

    refactored so it takes functions being used to process connect-4 game

--- spec/helperFuncs.spec.js
+++ spec/helperFuncs.spec.js
@@ -2,39 +2,27 @@ const {
   expect
 } = require('chai');
 
-const {checkWinnerTest} = require('./helpFuncsTest');
+const {checkWinner, changePlayer, addDisk} = require('../src/redux/helperFuncs');
 
 describe('check winner', () => {
   it('is a function', () => {
-    expect(checkWinnerTest).to.be.a('function');
+    expect(checkWinner).to.be.a('function');
   });
-  it('returns o as the winner', () => {
-    const board = [
-      [null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null],
-      [null, null, 'o', null, null, null, null],
-      [null, null, 'o', null, null, null, null],
-      [null, 'x', 'o', 'x', null, null, null],
-      [null, 'x', 'o', 'x', null, null, null]
-    ];
-    const board2 = [
-      [null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null],
-      [null, null, 'x', null, null, null, null],
-      [null, null, 'x', 'x', 'x', null, null],
-      [null, null, 'o', 'o', 'o', 'o', null]
-    ];
-        const board3 = [
-      [null, null, null, null, null, null, null],
-      [null, null, null, null, null, 'o', null],
-      [null, null, null, null, 'o', null, null],
-      [null, null, 'x', 'o', null, null, null],
-      [null, null, 'o', 'x', 'x', null, null],
-      [null, null, 'o', 'x', 'o', 'o', null]
-    ];
-    expect(checkWinnerTest(board)).to.equal('o wins');
-    expect(checkWinnerTest(board2)).to.equal('o wins');
-    expect(checkWinnerTest(board3)).to.equal('o wins');
+  it('empty board returns false as winner', () => {
+    const State = {
+      board: [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null]
+      ],
+      player: 'x',
+      won: false,
+      changePlayer: true,
+      plays: 0
+    };
+    expect(checkWinner(State).won).to.equal('o wins');
   });
 });
