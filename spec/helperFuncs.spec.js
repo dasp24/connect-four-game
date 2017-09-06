0e89547fe6fb10f789f6a16d14f93809102415a6
Dan Parkes <daniel.parkes901@gmail.com>
2017-09-06 15:00:28 +0100

    test the change player function

--- spec/helperFuncs.spec.js
+++ spec/helperFuncs.spec.js
@@ -4,9 +4,11 @@ const {
 
 const {checkWinner, changePlayer, addDisk} = require('../src/redux/helperFuncs');
 
-describe('check winner', () => {
-  it('is a function', () => {
+describe('connect-4 game', () => {
+  it('has functions - checkWinner, changePlayer and addDisk', () => {
     expect(checkWinner).to.be.a('function');
+    expect(changePlayer).to.be.a('function');
+    expect(addDisk).to.be.a('function');
   });
   it('empty board returns false as winner', () => {
     const State = {
@@ -23,6 +25,24 @@ describe('check winner', () => {
       changePlayer: true,
       plays: 0
     };
-    expect(checkWinner(State).won).to.equal('o wins');
+    expect(checkWinner(State).won).to.equal(false);
+    expect(checkWinner(State)).to.equal(State);
+  });
+  it('changes player correctly', () => {
+    const State = {
+      board: [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null]
+      ],
+      player: 'x',
+      won: false,
+      changePlayer: true,
+      plays: 0
+    };
+    expect(changePlayer(State.player)).to.equal('o');
   });
 });
